c826ff90739e90aac29f06d1a50ecf8a1fec6450
Taylor Plante <taylorplante99@gmail.com>
2018-12-07 16:56:14 +0100

    improved logic for moving things around and deselecting/reselecting items, styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { createRef } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 // Redux
-import { Box, BoxBorder, Select, Label } from "@mentimeter/ragnar-web";
+import { Box, BoxBorder, Label, Select, Text } from "@mentimeter/ragnar-web";
 
 const SKIP_VALUE = "skip";
 const ICON_SIZE = 25;
@@ -15,7 +15,16 @@ const ITEM_SIZE = GRID_SIZE * 2;
 const options = [
   { label: "Hillary Clinton" },
   { label: "George Washington" },
-  { label: "Barack Obama" }
+  { label: "Barack Obama" },
+  { label: "Bernie Sanders" },
+  { label: "John Adams" },
+  { label: "Marco Rubio" },
+  { label: "Donald Trump" },
+  { label: "Ron Swanson" },
+  { label: "Ron Burgandy" },
+  { label: "Abraham Lincoln" },
+  { label: "Jeb Bush" },
+  { label: "Kanye West" }
 ];
 
 // Reorder the result
@@ -36,7 +45,6 @@ const getItemStyle = (isDragging, draggableStyle) => ({
   // Styles to apply on draggables
   ...draggableStyle
 });
-
 const iconStyles = {
   opacity: 0,
   transition: "0.2s ease-in-out",
@@ -56,6 +64,22 @@ const optionStyles = {
   borderRadius: 3,
   margin: `${GUTTER_SIZE}px ${GRID_SIZE}px 0 ${GRID_SIZE}px`,
   padding: `${GUTTER_SIZE}px ${GRID_SIZE}px ${GUTTER_SIZE}px ${GUTTER_SIZE}px`
+};
+
+const ArrowIcon = ({ show }) => {
+  return (
+    <svg
+      style={{
+        ...iconStyles,
+        opacity: show ? 1 : 0
+      }}
+    >
+      <path
+        d="M13.7578943,16.6318165 L10.6642975,19.7254012 C10.2981644,20.0915329 9.70460909,20.0915329 9.33847592,19.7254012 L6.24487915,16.6318165 C5.65429263,16.0412323 6.07257429,15.0313915 6.90780948,15.0314306 L8.90632706,15.0314306 L8.90628799,11.0937468 L4.9686279,11.0937468 L4.9686279,13.0922566 C4.9686279,13.9274885 3.95882223,14.3457685 3.36819665,13.7551843 L0.274599878,10.6615996 C-0.0915332925,10.2954679 -0.0915332925,9.70187587 0.274599878,9.3357832 L3.36819665,6.24219851 C3.95878316,5.6516143 4.9686279,6.06989433 4.9686279,6.90512625 L4.9686279,8.9062532 L8.90628799,8.9062532 L8.90628799,4.96860849 L6.90511417,4.96860849 C6.06987897,4.96860849 5.65159732,3.95880676 6.24218383,3.36818349 L9.3357806,0.274598805 C9.70191377,-0.091532935 10.295469,-0.091532935 10.6616022,0.274598805 L13.755199,3.36818349 C14.3457855,3.9587677 13.9275038,4.96860849 13.0922686,4.96860849 L11.0937511,4.96860849 L11.0937511,8.9062532 L15.0314112,8.9062532 L15.0314112,6.90774343 C15.0314112,6.07251151 16.0412168,5.65423148 16.6318034,6.24481569 L19.7254001,9.33840038 C20.0915333,9.70453212 20.0915333,10.2981241 19.7254001,10.6642168 L16.6318034,13.7578015 C16.0412168,14.3483857 15.0313721,13.9301057 15.0314112,13.0948737 L15.0314112,11.0937468 L11.0937901,11.0937468 L11.0937901,15.0313915 L13.094964,15.0313915 C13.9301992,15.0313915 14.3484808,16.0411932 13.7578943,16.6318165 Z"
+        id="Path"
+      />
+    </svg>
+  );
 };
 
 class App extends React.PureComponent<Props, State> {
@@ -125,26 +149,33 @@ class App extends React.PureComponent<Props, State> {
         o => o.id === previousValue
       );
       newOptions[previousOptionIndex].selected = false;
+      if (value === SKIP_VALUE) {
+        const item = newItems[menuId];
+        item.showIcon = false;
+        item.val = SKIP_VALUE;
+        // Move deselected item to end of list
+        newItems.splice(menuId, 1);
+        newItems.push(item);
+      }
     }
     if (value !== SKIP_VALUE) {
       const optionIndex = newOptions.findIndex(o => o.id === value);
       newOptions[optionIndex].selected = true;
       newItems[menuId].showIcon = true;
       newItems[menuId].val = value;
-      if (!hasPreviousValue && menus.length < newOptions.length) {
+      const addItem =
+        !hasPreviousValue &&
+        menus.length < newOptions.length &&
+        menuId === newItems.length - 1;
+      if (addItem) {
         newItems.push(newOptions[menuId + 1]);
       }
-    } else if (hasPreviousValue) {
-      newItems[menuId].showIcon = false;
-      newItems[menuId].val = SKIP_VALUE;
       menus.forEach((menu, i) => {
-        const { value: menuValue } = menu;
-        if (i > menuId) {
-          newItems.pop();
-          const option = newOptions.find(o => o.id === menuValue);
-          if (option) {
-            option.selected = false;
-          }
+        const previousMenuWasSkipped = menu.value === SKIP_VALUE && i < menuId;
+        if (previousMenuWasSkipped) {
+          const item = newItems[i];
+          newItems.splice(i, 1);
+          newItems.push(item);
         }
       });
     }
@@ -159,95 +190,101 @@ class App extends React.PureComponent<Props, State> {
   render() {
     return (
       <div ref={this.container}>
-        <form onSubmit={this.handleSubmit}>
-          <DragDropContext onDragEnd={this.handleDragEnd}>
-            <Droppable droppableId="droppable">
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  style={{ display: "flex", flexDirection: "column" }}
-                >
-                  {this.state.items.map((item, i) => (
-                    <Draggable key={item.id} draggableId={item.id} index={i}>
-                      {(provided, snapshot) => (
-                        <div
-                          id={item.id}
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={{
-                            ...getItemStyle(
-                              snapshot.isDragging,
-                              provided.draggableProps.style
-                            ),
-                            ...optionStyles
-                          }}
-                          key={item.id}
-                        >
-                          {i < this.state.items.length && (
-                            <svg
-                              style={{
-                                ...iconStyles,
-                                opacity: item.showIcon ? 1 : 0
-                              }}
-                            >
-                              <path
-                                d="M13.7578943,16.6318165 L10.6642975,19.7254012 C10.2981644,20.0915329 9.70460909,20.0915329 9.33847592,19.7254012 L6.24487915,16.6318165 C5.65429263,16.0412323 6.07257429,15.0313915 6.90780948,15.0314306 L8.90632706,15.0314306 L8.90628799,11.0937468 L4.9686279,11.0937468 L4.9686279,13.0922566 C4.9686279,13.9274885 3.95882223,14.3457685 3.36819665,13.7551843 L0.274599878,10.6615996 C-0.0915332925,10.2954679 -0.0915332925,9.70187587 0.274599878,9.3357832 L3.36819665,6.24219851 C3.95878316,5.6516143 4.9686279,6.06989433 4.9686279,6.90512625 L4.9686279,8.9062532 L8.90628799,8.9062532 L8.90628799,4.96860849 L6.90511417,4.96860849 C6.06987897,4.96860849 5.65159732,3.95880676 6.24218383,3.36818349 L9.3357806,0.274598805 C9.70191377,-0.091532935 10.295469,-0.091532935 10.6616022,0.274598805 L13.755199,3.36818349 C14.3457855,3.9587677 13.9275038,4.96860849 13.0922686,4.96860849 L11.0937511,4.96860849 L11.0937511,8.9062532 L15.0314112,8.9062532 L15.0314112,6.90774343 C15.0314112,6.07251151 16.0412168,5.65423148 16.6318034,6.24481569 L19.7254001,9.33840038 C20.0915333,9.70453212 20.0915333,10.2981241 19.7254001,10.6642168 L16.6318034,13.7578015 C16.0412168,14.3483857 15.0313721,13.9301057 15.0314112,13.0948737 L15.0314112,11.0937468 L11.0937901,11.0937468 L11.0937901,15.0313915 L13.094964,15.0313915 C13.9301992,15.0313915 14.3484808,16.0411932 13.7578943,16.6318165 Z"
-                                id="Path"
-                              />
-                            </svg>
-                          )}
-                          <BoxBorder
-                            alignItems="center"
-                            justifyContent="center"
-                            width={ITEM_SIZE}
-                            height={ITEM_SIZE}
-                            bg="brand"
-                            mr={GUTTER_SIZE}
-                            borderRadius="50%"
+        <Box width="100%" height="100%" maxWidth="450px" m="0 auto">
+          <Box my={2} alignItems="center">
+            <Text
+              color="brand"
+              fontSize={5}
+              fontWeight="bold"
+              textAlign="center"
+            >
+              Choose your favorite candidates
+            </Text>
+          </Box>
+          <Box width="100%" alignItems="center">
+            <DragDropContext onDragEnd={this.handleDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided, snapshot) => (
+                  <div ref={provided.innerRef}>
+                    {this.state.items.map((item, i) => (
+                      <Draggable key={item.id} draggableId={item.id} index={i}>
+                        {(provided, snapshot) => (
+                          <div
+                            id={item.id}
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={{
+                              ...getItemStyle(
+                                snapshot.isDragging,
+                                provided.draggableProps.style
+                              ),
+                              ...optionStyles
+                            }}
                           >
-                            <Label value={i + 1} htmlFor={`menu-${i}`}>
-                              {i + 1}
-                            </Label>
-                          </BoxBorder>
-                          <Box
-                            flex={1}
-                            alignItems="center"
-                            justifyContent="center"
-                          >
-                            <Select
-                              id={`menu-${i}`}
-                              name={`menu-${i}`}
-                              onChange={this.handleChange}
+                            {i < this.state.items.length && (
+                              <ArrowIcon show={item.showIcon} />
+                            )}
+                            <BoxBorder
+                              alignItems="center"
+                              justifyContent="center"
+                              width={ITEM_SIZE}
+                              height={ITEM_SIZE}
+                              bg="brand"
+                              mr={GUTTER_SIZE}
+                              borderRadius="50%"
                             >
-                              <Select.Option
-                                key={SKIP_VALUE}
-                                id={SKIP_VALUE}
-                                value={SKIP_VALUE}
-                                defaultValue
+                              <Label
+                                value={i + 1}
+                                htmlFor={`menu-${i}`}
+                                color="white"
+                                mt={1}
                               >
-                                Select an option
-                              </Select.Option>
-                              {this.state.options.map(
-                                ({ label, id, selected }) =>
-                                  (!selected || item.val === id) && (
-                                    <Select.Option key={id} id={id} value={id}>
-                                      {label}
-                                    </Select.Option>
-                                  )
-                              )}
-                            </Select>
-                          </Box>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </form>
+                                {i + 1}
+                              </Label>
+                            </BoxBorder>
+                            <Box
+                              flex={1}
+                              alignItems="center"
+                              justifyContent="center"
+                            >
+                              <Select
+                                id={`menu-${i}`}
+                                name={`menu-${i}`}
+                                onChange={this.handleChange}
+                              >
+                                <Select.Option
+                                  id={SKIP_VALUE}
+                                  value={SKIP_VALUE}
+                                  defaultValue
+                                >
+                                  Select an option
+                                </Select.Option>
+                                {this.state.options.map(
+                                  ({ label, id, selected }) =>
+                                    (!selected || item.val === id) && (
+                                      <Select.Option
+                                        key={id}
+                                        id={id}
+                                        value={id}
+                                      >
+                                        {label}
+                                      </Select.Option>
+                                    )
+                                )}
+                              </Select>
+                            </Box>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </Box>
+        </Box>
       </div>
     );
   }
