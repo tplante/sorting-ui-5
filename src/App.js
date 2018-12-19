b662128626f0950d52c7b7edb7d8815815de20ae
Taylor Plante <taylorplante99@gmail.com>
2018-12-18 17:34:24 -0800

    cleanup and organize code, remove unused hunks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 // @flow
-import * as React from "react";
-import { createRef } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-
-// Redux
+import React, { createRef } from "react";
 import {
   Box,
   BoxBorder,
@@ -13,14 +9,16 @@ import {
   Text
 } from "@mentimeter/ragnar-web";
 import { CrossIcon } from "@mentimeter/ragnar-visuals";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const SKIP_VALUE = "skip";
-const DEFAULT_OPTION = "Select an option";
+const PLACEHOLDER_VALUE = "Select an option";
 const ICON_SIZE = 20;
-const ICON_MARGIN = 5;
+const ICON_MARGIN = 10;
 const GRID_SIZE = 20;
 const GUTTER_SIZE = GRID_SIZE / 2;
 const ITEM_SIZE = GRID_SIZE * 2;
+const BORDER_WIDTH = 2;
 const SHADE_2 = "rgb(195, 200, 213)";
 const SHADE_3 = "rgb(37, 43, 54)";
 
@@ -39,64 +37,61 @@ const options = [
   { label: "Kanye West" }
 ];
 
-// Reorder the result
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
-
   return result;
 };
-const getItemStyle = (isDragging, draggableStyle, isLastItem) => ({
+const getIndexFromId = string => parseInt(string.split("-")[1]); // e.g., "option-10" -> "10"
+const getItemStyles = (isDragging, draggableStyle, isLastItem) => ({
   position: "relative",
   userSelect: "none",
   overflow: "visible",
   border:
-    isDragging || isLastItem ? `2px dashed ${SHADE_2}` : `2px solid ${SHADE_2}`,
+    isDragging || isLastItem
+      ? `${BORDER_WIDTH}px dashed ${SHADE_2}`
+      : `${BORDER_WIDTH}px solid ${SHADE_2}`,
   // Styles to apply on draggables
   ...draggableStyle
 });
 const moveIconStyles = {
   position: "absolute",
-  left: -ICON_SIZE - 2 * ICON_MARGIN,
-  padding: `${GRID_SIZE}px ${ICON_MARGIN}px`
+  left: -ICON_SIZE - ICON_MARGIN,
+  padding: `${GRID_SIZE}px ${ICON_MARGIN / 2}px`
 };
 
-const ArrowIcon = () => {
-  return (
-    <svg
-      stroke={SHADE_3}
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      width={ICON_SIZE}
-      height={ICON_SIZE}
-      viewBox={`0 0 ${ICON_SIZE} ${ICON_SIZE}`}
-      style={moveIconStyles}
-    >
-      <polyline points="3.7 7.3 1 10 3.7 12.7" />
-      <polyline points="7.3 3.7 10 1 12.7 3.7" />
-      <polyline points="12.7 16.3 10 19 7.3 16.3" />
-      <polyline points="16.3 7.3 19 10 16.3 12.7" />
-      <path d="M1,10 L19,10" />
-      <path d="M10,1 L10,19" />
-    </svg>
-  );
-};
-
-const PlusIcon = () => (
+const Icon = ({ children, stroke, style = {} }) => (
   <svg
-    stroke="white"
-    strokeWidth="2"
+    stroke={stroke}
+    strokeWidth={BORDER_WIDTH}
     strokeLinecap="round"
     strokeLinejoin="round"
     width={ICON_SIZE}
     height={ICON_SIZE}
     viewBox={`0 0 ${ICON_SIZE} ${ICON_SIZE}`}
+    style={style}
   >
-    <path d="M10,1 L10,19" />
-    <path d="M1,10 L19,10" />
+    {children}
   </svg>
+);
+
+const ArrowIcon = () => (
+  <Icon stroke={SHADE_3} style={moveIconStyles}>
+    <polyline points="3.7 7.3 1 10 3.7 12.7" />
+    <polyline points="7.3 3.7 10 1 12.7 3.7" />
+    <polyline points="12.7 16.3 10 19 7.3 16.3" />
+    <polyline points="16.3 7.3 19 10 16.3 12.7" />
+    <path d="M1,10 L19,10" />
+    <path d="M10,1 L10,19" />
+  </Icon>
+);
+
+const PlusIcon = () => (
+  <Icon stroke="white">
+    <path d="M1,10 L19,10" />
+    <path d="M10,1 L10,19" />
+  </Icon>
 );
 
 class App extends React.PureComponent<Props, State> {
@@ -112,6 +107,13 @@ class App extends React.PureComponent<Props, State> {
     this.keyGenerationIndex = 1;
     this.setOptions(options);
   }
+
+  getPlaceholderItem = (options, menuId) => {
+    const placeholderItem = { ...options[menuId + 1] }; // Copy of next option, prevent mutation
+    placeholderItem.id = `option-${options.length + this.keyGenerationIndex}`;
+    this.keyGenerationIndex++;
+    return placeholderItem;
+  };
 
   setOptions = choices => {
     const options = choices.map((c, i) => ({
@@ -157,8 +159,6 @@ class App extends React.PureComponent<Props, State> {
     } = event;
     const options = [...this.state.options]; // Prevent mutation
     const items = [...this.state.items]; // Prevent mutation
-    const menuId = parseInt(id.split("-")[1]); // e.g., "option-10" -> "10"
-    const selectedItem = items[menuId];
     // Option was selected before
     if (previousValue) {
       const previousOption = options.find(o => o.id === previousValue);
@@ -168,14 +168,16 @@ class App extends React.PureComponent<Props, State> {
     if (value !== SKIP_VALUE) {
       const option = options.find(o => o.id === value);
       option.selected = true;
+      const menuId = getIndexFromId(id);
+      const selectedItem = items[menuId];
       selectedItem.val = value;
       const shouldAddItem =
-        items.length < options.length && menuId === items.length - 1; // Still menus left to append // Is the last menu item currently rendered
+        items.length < options.length && menuId === items.length - 1; // Still menus left to append
       if (shouldAddItem) {
         const placeholderItem = this.getPlaceholderItem(options, menuId);
         items.push(placeholderItem);
       }
-      const itemsAreMoveable = items.length > 2;
+      const itemsAreMoveable = items.length > 2; // Need 2 or more selected items for dnd to be useful
       if (itemsAreMoveable) {
         items.forEach(item => (item.showMovableIcon = item.val !== SKIP_VALUE));
       }
@@ -184,20 +186,12 @@ class App extends React.PureComponent<Props, State> {
     this.setState({ options, items });
   };
 
-  getPlaceholderItem = (options, menuId) => {
-    const placeholderItem = { ...options[menuId + 1] }; // Copy of next option, prevent mutation
-    placeholderItem.id = `option-${options.length + this.keyGenerationIndex}`;
-    this.keyGenerationIndex++;
-    return placeholderItem;
-  };
-
   handleDeselect = event => {
     const { id } = event.currentTarget; // Selects "this" element instead of any of its children
     const options = [...this.state.options]; // Prevent mutation
     const items = [...this.state.items]; // Prevent mutation
-    const menuId = parseInt(id.split("-")[1]);
-    const item = items[menuId];
-    const itemId = parseInt(item.val.split("-")[1]);
+    const menuId = getIndexFromId(id);
+    const itemId = getIndexFromId(items[menuId].val);
     const option = options[itemId];
     if (!option) {
       return;
@@ -211,8 +205,6 @@ class App extends React.PureComponent<Props, State> {
     });
     // Move deselected item to end of list
     const placeholderItem = this.getPlaceholderItem(options, menuId);
-    placeholderItem.val = SKIP_VALUE;
-    placeholderItem.showMovableIcon = false;
     items.splice(menuId, 1);
     items.push(placeholderItem);
     this.setState({ options, items });
@@ -267,7 +259,7 @@ class App extends React.PureComponent<Props, State> {
                             borderRadius={1}
                             p={`${GUTTER_SIZE}px 0 ${GUTTER_SIZE}px ${GUTTER_SIZE}px`}
                             style={{
-                              ...getItemStyle(
+                              ...getItemStyles(
                                 snapshot.isDragging,
                                 provided.draggableProps.style,
                                 isLastItem(item, i)
@@ -310,7 +302,7 @@ class App extends React.PureComponent<Props, State> {
                                   defaultValue
                                   disabled={item.val !== SKIP_VALUE}
                                 >
-                                  {DEFAULT_OPTION}
+                                  {PLACEHOLDER_VALUE}
                                 </Select.Option>
                                 {this.state.options.map(
                                   ({ label, id, selected }) =>
